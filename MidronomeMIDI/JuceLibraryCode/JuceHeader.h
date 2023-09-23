1c95f7dae4f9988add79504f87a31debe542bbf3
Simon Lasnier <simon@midronome.com>
2023-09-23 11:06:50 +0200

    MidronomeMIDI plugin: background with logo

--- MidronomeMIDI/JuceLibraryCode/JuceHeader.h
+++ MidronomeMIDI/JuceLibraryCode/JuceHeader.h
@@ -26,6 +26,7 @@
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_gui_extra/juce_gui_extra.h>
 
+#include "BinaryData.h"
 
 #if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
  /** If you've hit this error then the version of the Projucer that was used to generate this project is
@@ -42,7 +43,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "MidronomeMIDI";
     const char* const  companyName    = "Midronome ApS";
-    const char* const  versionString  = "1.0.1";
-    const int          versionNumber  = 0x10001;
+    const char* const  versionString  = "1.1.0";
+    const int          versionNumber  = 0x10100;
 }
 #endif
